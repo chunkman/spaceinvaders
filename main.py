6272c52d8621b5d769dfe61b0b0e49e296260490
Ken Allen <chunkman@gmail.com>
2020-11-11 11:19:20 -0500

    Added player movement and spawn single enemy.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # My pygame crap
 
 import pygame
+import random
 
 # Initialize pygame
 
@@ -18,10 +19,24 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('space-invaders.png')
 playerX = 370
 playerY = 480
+playerX_change = 0
+playerY_change = 0
+
+
+# Enemy
+enemyImg = pygame.image.load('alien_enemy.png')
+enemyX = random.randint(0, 800)
+enemyY = random.randint(50, 150)
+enemyX_change = 0
+enemyY_change = 0
 
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
+
+
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
 
 
 # Game Loop
@@ -30,10 +45,26 @@ while running:
 
     # RGB - Red, Green, Blue
     screen.fill((0, 0, 0))
-    
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+        # Check for left or right keystroke
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    playerX += playerX_change
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
     player(playerX, playerY)
+    enemy(enemyX, enemyY)
     pygame.display.update()
